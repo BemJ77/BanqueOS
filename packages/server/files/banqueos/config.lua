9e78013de09bd8572a7d86dea8669bc6ae07f661
BemJ77 <benjamin.breteche@sfr.fr>
2026-07-31 19:15:30 +0200

    update(serveur): update requiredAtmVersion

--- packages/server/files/banqueos/config.lua
+++ packages/server/files/banqueos/config.lua
@@ -17,7 +17,7 @@ return {
 
     -- Communication sans fil BANQUEOS.
     networkProtocol = "banqueos.network.v1",
-    requiredAtmVersion = "0.2.1",
+    requiredAtmVersion = "0.2.2",
     atmStartupScanSeconds = 2.5,
     atmPingTimeoutSeconds = 1.2,
 }
